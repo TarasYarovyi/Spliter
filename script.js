b262e79701c599277932189c48245e42ab0fd0dc
TarasYarovyi <yarovoy.taras@gmail.com>
2023-05-23 17:32:21 +0200

    Script completed

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ const people = document.querySelector("#people");
 const additionalCosts = document.querySelector("#additionalCosts");
 const count = document.querySelector(".count");
 const cost = document.querySelector(".cost");
+const costInfo = document.querySelector(".cost-info");
+const error = document.querySelector(".error");
 
 count.addEventListener("click", countCost);
 function countCost() {
   if (amount.value && additionalCosts.value && people.value) {
     const splitedCost = (amount.value * additionalCosts.value) / people.value;
-    cost.textContent = splitedCost;
+    cost.textContent = splitedCost.toFixed(2);
+    error.style.display = "none";
+    costInfo.style.display = "block";
   } else {
-    document.querySelector(".error").textContent =
-      "Please complete all required information";
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".cost-info").style.display = "none";
+    error.textContent = "Please complete all required information";
+    error.style.display = "block";
+    costInfo.style.display = "none";
   }
 }
